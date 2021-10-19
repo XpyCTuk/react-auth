d1493ff57c055fb7b80429fc033cfbe70706b247
XpyCTuk <gon4arov@gmail.com>
2021-10-19 14:35:09 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import ContactFormComponent from './Components/contact_form_component';
 import Autho from './Components/Autho';
 import TrueUser from './Components/TrueUser';
 
 const status = true
 
 function App() {
+
   return (
-    // <ContactFormComponent />
     <>
       <Autho />
       <TrueUser />
