b63437ae78925efd3ad59cff6291c06b956ff906
Denis <pelevin.d.y@gmail.com>
2021-10-19 14:56:10 +0300

    feat: components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Autho from './Components/Autho';
-import TrueUser from './Components/TrueUser';
-
-const status = true
+import React, { useState } from 'react'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Autho from './Components/Autho'
+import TrueUser from './Components/TrueUser'
 
 function App() {
+  const [status] = useState(true)
 
   return (
     <>
-      <Autho />
-      <TrueUser />
+      {status === true && <Autho />}
+      {status === false && <TrueUser />}
     </>
-  );
+  )
 }
 
-export default App;
-
-
+export default App
